c38bededf1e350f84dc362090ba7f2f7d1134355
nicolevaleria20 <58374803+nicolevaleria20@users.noreply.github.com>
2020-03-19 12:57:32 -0600

    Creacion clases
    Se agregaron las clases pero faltaría agregar más
    
    Co-Authored-By: pjbc9716 <pjbc9716@users.noreply.github.com>

---
+++ ProyectoBiblioteca/Administrador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoBiblioteca
+{
+    class Administrador: Empleado // Se llama a la clase padre conocido como Empleado
+    {
+
+        // Atributos de la clase Administrador
+        private int id_administrador;
+        private String username;
+        private String contrasena;
+        private DateTime ultimo_ingreso;
+
+        // Constructor
+        public Administrador(int cedula_empleado, string username, string contrasena, DateTime ultimo_ingreso, string primer_nombre, string segundo_nombre, 
+            string primer_apellido, string segundo_apellido, string genero, string tipo_puesto): base (cedula_empleado, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, genero, tipo_puesto)
+        {
+            this.id_administrador = cedula_empleado;
+            this.username = username;
+            this.contrasena = contrasena;
+            this.ultimo_ingreso = ultimo_ingreso;
+        }
+
+
+
+    }
+}
---
+++ ProyectoBiblioteca/Usuario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoBiblioteca
+{
+    class Usuario
+    {
+        // Atributos de la clase Usuario
+        private int cedula_usuario;
+        private String primer_nombre_usuario;
+        private String primer_apellido_usuario;
+        private String segundo_apellido_usuario;
+        private String correo_usuario;
+        private int telefono_usuario;
+        private String provincia;
+        private String canton;
+        private String distrito;
+
+        // Constructor
+        public Usuario(int cedula_usuario, string primer_nombre_usuario, string primer_apellido_usuario, string segundo_apellido_usuario, string correo_usuario, int telefono_usuario, string provincia, string canton, string distrito)
+        {
+            this.cedula_usuario = cedula_usuario;
+            this.primer_nombre_usuario = primer_nombre_usuario;
+            this.primer_apellido_usuario = primer_apellido_usuario;
+            this.segundo_apellido_usuario = segundo_apellido_usuario;
+            this.correo_usuario = correo_usuario;
+            this.telefono_usuario = telefono_usuario;
+            this.provincia = provincia;
+            this.canton = canton;
+            this.distrito = distrito;
+        }
+    }
+}
